a51b8461f3be0d5b47c7287447a4f086cfb21e47
Adrian Jones <adrianjones@duck.com>
2023-07-18 10:33:44 -0700

    inital commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const theDiv = document.getElementById("theDiv");
+
+const frame = document.createElement("div");
+frame.classList.add("frame");
+frame.style.width = "350px";
+frame.style.height = "500px";
+frame.style.margin = "auto";
+frame.style["margin-top"] = "100px";
+frame.style["background-color"] = "grey";
+frame.style.padding = "50px";
+
+const screen = document.createElement("div");
+screen.classList.add("screen");
+screen.style.width = "450px"
+screen.style["background-color"] = "yellow";
+screen.style.width = "300px";
+screen.style.height = "80px";
+screen.style.margin = "auto";
+screen.style.padding = "10px";
+
+frame.appendChild(screen);
+theDiv.appendChild(frame);
+
